a9b51bfb6ac50f85de67c89cc6811b2df2f00648
Ho Zhi Ying Michelle <hozhiying@Hos-MacBook-Pro.local>
2022-03-02 09:12:58 +0800

    new

---
+++ src/main/Workshop12Application.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.workshop12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringApplication;
+
+@SpringBootApplication
+public class Workshop12Application {
+    
+    public static void main (String[] args){
+        SpringApplication.run(Workshop12Application.class, args);
+    }
+}
